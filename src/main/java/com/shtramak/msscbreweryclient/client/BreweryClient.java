0281071793b700df8db2e1dd3aa2e73c2adff1ce
shtramak <shtramak@gmail.com>
2019-09-22 12:49:04 +0300

    add RestTemplate get, post,put,delete methods + tests for Customer

--- src/main/java/com/shtramak/msscbreweryclient/client/BreweryClient.java
+++ src/main/java/com/shtramak/msscbreweryclient/client/BreweryClient.java
@@ -1,6 +1,7 @@
 package com.shtramak.msscbreweryclient.client;
 
 import com.shtramak.msscbreweryclient.model.BeerDto;
+import com.shtramak.msscbreweryclient.model.CustomerDto;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -14,6 +15,7 @@ import java.util.UUID;
 @ConfigurationProperties(prefix = "brewery.client", ignoreUnknownFields = false)
 public class BreweryClient {
     private final String BEER_PATH = "/api/v1/beers/";
+    private final String CUSTOMER_PATH = "/api/v1/customers/";
     @Setter
     private String host;
 
@@ -23,19 +25,35 @@ public class BreweryClient {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public BeerDto getBeerById(UUID beerId){
+    public BeerDto getBeerById(UUID beerId) {
         return restTemplate.getForObject(host + BEER_PATH + beerId, BeerDto.class);
     }
 
-    public URI postBeer(BeerDto beerDto){
+    public CustomerDto getCustomerById(UUID customerId) {
+        return restTemplate.getForObject(host + CUSTOMER_PATH + customerId, CustomerDto.class);
+    }
+
+    public URI postBeer(BeerDto beerDto) {
         return restTemplate.postForLocation(host + BEER_PATH, beerDto);
     }
 
-    public void updateBeer(BeerDto beerDto){
-        restTemplate.put(host+BEER_PATH+beerDto.getId(),beerDto);
+    public URI postCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(host + CUSTOMER_PATH, customerDto);
     }
 
-    public void deleteBeerById(UUID beerID){
-        restTemplate.delete(host+BEER_PATH+beerID);
+    public void updateBeer(BeerDto beerDto) {
+        restTemplate.put(host + BEER_PATH + beerDto.getId(), beerDto);
+    }
+
+    public void updateCustomer(CustomerDto customerDto) {
+        restTemplate.put(host + CUSTOMER_PATH + customerDto.getId(), customerDto);
+    }
+
+    public void deleteBeerById(UUID beerId) {
+        restTemplate.delete(host + BEER_PATH + beerId);
+    }
+
+    public void deleteCustomerById(UUID customerId) {
+        restTemplate.delete(host + CUSTOMER_PATH + customerId);
     }
 }
--- src/test/java/com/shtramak/msscbreweryclient/MsscBreweryClientApplicationTests.java
+++ src/test/java/com/shtramak/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -2,6 +2,7 @@ package com.shtramak.msscbreweryclient;
 
 import com.shtramak.msscbreweryclient.client.BreweryClient;
 import com.shtramak.msscbreweryclient.model.BeerDto;
+import com.shtramak.msscbreweryclient.model.CustomerDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,26 +22,50 @@ public class MsscBreweryClientApplicationTests {
     BreweryClient breweryClient;
 
     @Test
-    public void getByIdWhenValidRequestReturnsBeerDto() {
+    public void getBeerByIdWhenValidRequestReturnsBeerDto() {
         BeerDto beerById = breweryClient.getBeerById(UUID.randomUUID());
         assertNotNull(beerById);
     }
 
     @Test
-    public void postWhenValidRequestReturnsURI(){
+    public void postBeerWhenValidRequestReturnsURI(){
         BeerDto beerDto = BeerDto.builder().build();
         URI uri = breweryClient.postBeer(beerDto);
         System.out.println(uri);
     }
 
     @Test
-    public void putWhenValidRequestHasSuccessStatus(){
+    public void updateBeerWhenValidRequestHasSuccessStatus(){
         BeerDto beerDto = BeerDto.builder().id(UUID.randomUUID()).build();
         breweryClient.updateBeer(beerDto);
     }
 
     @Test
-    public void deleteWhenValidRequestHasSuccessStatus(){
+    public void deleteBeerWhenValidRequestHasSuccessStatus(){
         breweryClient.deleteBeerById(UUID.randomUUID());
+    }
+
+    @Test
+    public void getCustomerByIdWhenValidRequestReturnsBeerDto() {
+        CustomerDto customerDto = breweryClient.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    public void postCustomerWhenValidRequestReturnsURI(){
+        CustomerDto customerDto = CustomerDto.builder().build();
+        URI uri = breweryClient.postCustomer(customerDto);
+        System.out.println(uri);
+    }
+
+    @Test
+    public void updateCustomerWhenValidRequestHasSuccessStatus(){
+        CustomerDto customerDto = CustomerDto.builder().id(UUID.randomUUID()).build();
+        breweryClient.updateCustomer(customerDto);
+    }
+
+    @Test
+    public void deleteWhenValidRequestHasSuccessStatus(){
+        breweryClient.deleteCustomerById(UUID.randomUUID());
     }
 }
